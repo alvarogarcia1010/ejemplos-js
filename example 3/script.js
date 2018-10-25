753e902f32971ae96eeb7744844a859b56d9b5d6
alvarogarcia1010 <00111317@uca.edu.sv>
2018-10-25 15:09:56 -0600

    haciendo pruebas

--- example 3/script.js
+++ example 3/script.js
@@ -1,20 +1,15 @@
 // Object Literal
 var person = {
     /* property: value*/
-    name: "Néstor",
-    lastname: "Aldana",
-    birthday: Date.now()
+    name: "Alvaro",
+    lastname: "Garcia",
+    birthday: new Date(1998, 8, 10)
 }; // JSON (JavaScript Object Notation)
 
 // Access to propertys
 console.log(person.name);
-// Change object's property value
-person.birthday = new Date(1994, 0, 11); // 11 - Jan - 1994
 console.log(person.birthday);
-
-console.log(person.dui) // undefined
-person.dui = "000000000" // Assign 
-console.log(person.dui) // 000000000
+person.dui = "000000000" // Assign
 
 // Example
 function createPerson(name, lastname, birthday, dui) {
@@ -33,32 +28,21 @@ for (let i = 0; i < 10; i++) {
     list.push(createPerson(`Name ${i}`, `Lastname ${i}`, new Date().setFullYear(1990 + i + Math.floor(Math.random() * 5)), `000000${i}`));
 }
 
-console.table(list);
-
-// Array Higher function
-// Show only the name persons
-console.table(list.map(({
-    name
-}) => name));
+//console.table(list);
 
 // Get average age
-console.log("Average age %i", list.reduce((sum, {
-    birthday
-}) => {
-    var ageDifMs = Date.now() - birthday;
-    var ageDate = new Date(ageDifMs); // miliseconds from epoch
-    return Math.abs(ageDate.getUTCFullYear() - 1970) + sum;
-}, 0) / list.length);
-
-
-// More readable
-
 function getAge(birthday) {
     var ageDifMs = Date.now() - birthday;
+    console.log(ageDifMs);
     var ageDate = new Date(ageDifMs); // miliseconds from epoch
+    var miliYear = 1000*60*60*24*365;
+    console.log(ageDate/miliYear);
+
     return Math.abs(ageDate.getUTCFullYear() - 1970);
 }
 
-console.log("Average age %i", list.reduce((sum, {
-    birthday
-}) => getAge(birthday) + sum, 0) / list.length);
+console.log(getAge(person.birthday))
+
+// console.log("Average age %i", list.reduce((sum, {
+//     birthday
+// }) => getAge(birthday) + sum, 0) / list.length);
